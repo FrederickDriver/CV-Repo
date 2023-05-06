0277766ad1a85fbc9f1f7a95e916a1ba822b6d5d
UberFood <59709698+UberFood@users.noreply.github.com>
2023-05-06 15:37:37 -0400

    Label fixing routine

--- yolov5_mod/fix_labels.py
+++ yolov5_mod/fix_labels.py
@@ -12,10 +12,10 @@ for filename in os.listdir(directory):
             image_path = image_directory + pure_name + ".png"
             im = Image.open(image_path)
             w, h = im.size
-            #words[1] =  str(float(words[1])/w)
-            #words[3] =  str(float(words[3])/w)
-            #words[2] =  str(float(words[2])/h)
-            #words[4] =  str(float(words[4])/h)
+            words[1] =  str(float(words[1])/w)
+            words[3] =  str(float(words[3])/w)
+            words[2] =  str(float(words[2])/h)
+            words[4] =  str(float(words[4])/h)
             words[1] =  str(float(words[1]) + float(words[3])/2)
             words[2] =  str(float(words[2]) + float(words[4])/2)
             to_write = words[0] + " " + words[1] + " " + words[2] + " " + words[3] + " " + words[4]
